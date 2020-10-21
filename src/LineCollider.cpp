0bd44aa1fccacf07820026244f64bf3e2307f986
Gon <gon27.tk08@gmail.com>
2020-10-21 23:42:30 +0900

    線と矩形の当たり判定を追加

--- src/LineCollider.cpp
+++ src/LineCollider.cpp
@@ -8,6 +8,21 @@
 
 namespace gnCollider2D {
 
+    float cross(float _x1, float _y1, float _x2, float _y2)
+    {
+        return _x1 * _y2 - _x2 * _y1;
+    }
+
+    bool hitLine(float ax, float ay, float bx, float by, float cx, float cy, float dx, float dy)
+    {
+        auto t1{(cx - dx) * (ay - cy) + (cy - dy) * (cx - ax)};
+        auto t2{(cx - dx) * (by - cy) + (cy - dy) * (cx - bx)};
+        auto t3{(ax - bx) * (cy - ay) + (ay - by) * (ax - cx)};
+        auto t4{(ax - bx) * (dy - ay) + (ay - by) * (ax - dx)};
+
+        return t3 * t4 <= 0 && t1 * t2 <= 0;
+    }
+
     LineCollider::LineCollider()
         : start()
         , end()
@@ -25,12 +40,21 @@ namespace gnCollider2D {
         return ColliderType::LINE;
     }
 
-    float cross(float _x1, float _y1, float _x2, float _y2){
-        return _x1 * _y2 - _x2 * _y1;
-    }
-
     bool LineCollider::isHitTest(const BoxCollider& _collider) {
         const auto& bounds = _collider.getBounds();
+
+        float ax{ bounds.minPos.x }, ay{ bounds.minPos.y }, bx{ bounds.minPos.x }, by{ bounds.maxPos.y };
+        float cx{ start.x }, cy{ start.y }, dx{ end.x }, dy{ end.y };
+        return hitLine(ax, ay, bx, by, cx, cy, dx, dy);
+
+        ax = bounds.minPos.x; ay = bounds.minPos.y; bx = bounds.maxPos.x; by = bounds.minPos.y;
+        return hitLine(ax, ay, bx, by, cx, cy, dx, dy);
+
+        ax = bounds.maxPos.x; ay = bounds.minPos.y; bx = bounds.maxPos.x;by = bounds.maxPos.y;
+        return hitLine(ax, ay, bx, by, cx, cy, dx, dy);
+
+        ax = bounds.minPos.x; ay = bounds.maxPos.y; bx = bounds.maxPos.x; by = bounds.maxPos.y;
+        return hitLine(ax, ay, bx, by, cx, cy, dx, dy);
 
         return false;
     }
@@ -71,12 +95,7 @@ namespace gnCollider2D {
         float ax{ start.x     }, ay{ start.y     }, bx{ end.x     }, by{ end.y     };
         float cx{ col.start.x }, cy{ col.start.y }, dx{ col.end.x }, dy{ col.end.y };
 
-        auto t1 { (cx - dx) * (ay - cy) + (cy - dy) * (cx - ax) };
-        auto t2 { (cx - dx) * (by - cy) + (cy - dy) * (cx - bx) };
-        auto t3 { (ax - bx) * (cy - ay) + (ay - by) * (ax - cx) };
-        auto t4 { (ax - bx) * (dy - ay) + (ay - by) * (ax - dx) };
-
-        return t3 * t4 <= 0 && t1 * t2 <= 0;
+        return hitLine(ax, ay, bx, by, cx, cy, dx, dy);
     }
 
     bool LineCollider::isHitTest(const PointCollider& _collider) {
